24b53635cee8e9ae6165ccef94c0d2a1b15254e9
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2014-01-27 10:12:52 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmoneyServer::Application.config.secret_key_base = 'a25118f8afe4d6236245bd4cfb16bf30bc84f8288d4214ae61757dbad4d19c9516689a6b0d66c370cb265552d3d09249f625a3646c5f822a60a49f5847672c66'
