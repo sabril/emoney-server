0465caff5bb7c9f28b53876a2179c9debc8d74f0
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2014-04-14 07:38:39 +0700

    update account users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,3 +1,9 @@
 class Admin::UsersController < InheritedResources::Base
   before_filter :authenticate_user!
+  load_and_authorize_resource :user
+  
+  protected
+  def permitted_params
+    params.permit(user: [:email, :first_name, :last_name, :phone, :identity_number, :password, :password_confirmation])
+  end
 end
